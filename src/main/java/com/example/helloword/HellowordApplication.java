4013f03cf7141d14e6486ed80623157df63e7aa0
t-vinai <86148706+t-vinai@users.noreply.github.com>
2026-03-22 21:18:21 +0700

    ADD COMMAND LINE RUNNER

--- src/main/java/com/example/helloword/HellowordApplication.java
+++ src/main/java/com/example/helloword/HellowordApplication.java
@@ -1,13 +1,23 @@
 package com.example.helloword;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HellowordApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HellowordApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            System.out.println("===> Hello World from CommandLineRunner...");
+        };
     }
 
 }
